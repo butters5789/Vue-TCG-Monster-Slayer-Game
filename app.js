e81ba22869017ae0eda5dc8a3a633c8e57ff36fb
butters5789 <butters5789@gmail.com>
2022-07-28 10:32:17 -0600

    add special attack

--- app.js
+++ app.js
@@ -10,9 +10,13 @@ vueApp = createApp({
     return {
       monsterHealth: 100,
       playerHealth: 100,
+      attackRound: 0,
     };
   },
   computed: {
+    disableSpecialAttack() {
+      return this.attackRound % 3 !== 0;
+    },
     monsterHealthBarStyles() {
       return { width: `${this.monsterHealth}%` };
     },
@@ -25,10 +29,17 @@ vueApp = createApp({
       const damage = getAttackValue(12, 5);
       this.monsterHealth = this.monsterHealth - damage;
       this.attackPlayer();
+      this.attackRound = this.attackRound + 1;
     },
     attackPlayer() {
       const damage = getAttackValue(15, 8);
       this.playerHealth = this.playerHealth - damage;
+    },
+    specialAttack() {
+      const damage = getAttackValue(10, 25);
+      this.monsterHealth = this.monsterHealth - damage;
+      this.attackPlayer();
+      this.attackRound = this.attackRound + 1;
     },
   },
 });
